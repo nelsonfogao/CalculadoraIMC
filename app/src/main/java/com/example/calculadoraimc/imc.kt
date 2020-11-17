b7fb3904bbaf9b99076245c1d92aac6237bf16b5
eloybarbosa <eloy.barbosa@al.infnet.edu.br>
2020-11-16 22:25:13 -0300

    Front da Calculadora de IMC - Ajustes

--- app/src/main/java/com/example/calculadoraimc/imc.kt
+++ app/src/main/java/com/example/calculadoraimc/imc.kt
@@ -7,5 +7,7 @@ class imc : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
+
+
     }
 }
