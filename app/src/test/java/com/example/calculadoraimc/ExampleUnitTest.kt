fa95183dba11b2056381946d1a26aeb644a2225a
Nelson Barros <nelsonfogao@users.noreply.github.com>
2020-12-15 00:15:57 -0300

    teste

--- app/src/test/java/com/example/calculadoraimc/ExampleUnitTest.kt
+++ app/src/test/java/com/example/calculadoraimc/ExampleUnitTest.kt
@@ -1,8 +1,11 @@
 package com.example.calculadoraimc
 
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.Assert.assertThat
+import org.junit.Before
 import org.junit.Test
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +13,27 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    private var mCalculator: Calculator? = null
+
+    /**
+     * Set up the environment for testing
+     */
+    @Before
+    fun setUp() {
+        mCalculator = Calculator()
+    }
+
+    /**
+     * Test for simple addition
+     */
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun multTwoNumbers() {
+        val resultMult = mCalculator!!.mul(1.70, 1.70)
+        assertThat(resultMult, `is`(equalTo(2.89)))
+    }
+    @Test
+    fun divTwoNumbers() {
+        val resultDiv = mCalculator!!.div(78.0, 2.89)
+        assertThat(resultDiv, `is`(equalTo(26)))
     }
 }
